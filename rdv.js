5f00dab2e3487a36b237f54d96b0419c96ab4766
Jeff Hicks <jmhicks@gmail.com>
2014-02-17 17:57:46 -0500

    Work in progress
    Change to vector layer instead of markers
    Logic is nearly complete with linking an incrementing label to the graph
    dev/ is a temporary place for quicker iterative changes - will be removed later (and the gulp file will be updated accordingly)

--- rdv.js
+++ rdv.js
@@ -4,6 +4,9 @@ var TEMPLATE_LOCATION = 'detail.tpl.html';
 var DETAIL_TEMPLATE;
 var PANEL_STATION_COUNT = 0;
 var LABEL_OFFSET = 14;
+var MAX_SELECTED_STATIONS = 6;
+
+var stationAndGraphLinkHash = {};
 
 $(function(){
     $.get( TEMPLATE_LOCATION, function( template ) {
@@ -33,65 +36,87 @@ function deployMap() {
                 'static/stations1.json',
                 'Stream Gauges',
                 MARKER_COLORS.BLUE,
-                'EPSG:4326',
-                clickPoint
+                'EPSG:4326'
             ),
             new MapliteDataSource(
                 'static/stations2.json',
                 'Precipitation Gauges',
                 MARKER_COLORS.GREEN,
-                'EPSG:4326',
-                clickPoint
+                'EPSG:4326'
             )
         ],
-        iconPath: BUILD_BASE_PATH + 'img/'
+        iconPath: BUILD_BASE_PATH + 'img/',
+        selectCallback: clickPoint
     });
 }
 
-function clickPoint( marker, point ) {
-    var selected = $(marker.icon.imageDiv).attr('selected');
-    if ( !selected ) {
-        var contents = Mustache.render(
-                DETAIL_TEMPLATE, {
-                    id: sanitizeString( point.id ),
-                    name: point.name.toCapitalCase(),
-                    lat: point.lat,
-                    lon: point.lon
-                }
-        );
-        
-        /* WIP: label selected items - this method is probably not feasible given
-         * that it doesn't zoom in/out well. May need to actually use a feature label.
-
-        var markerRef = $(marker.icon.imageDiv);
-        var leftStr = markerRef.css( 'left' );
-        var left = parseInt( leftStr.substring( 0, leftStr.length - 2 ) ) + LABEL_OFFSET;
-        var topStr = markerRef.css( 'top' );
-        var top = parseInt( topStr.substring( 0, topStr.length - 2 ) ) - LABEL_OFFSET;
-        var label = ++PANEL_STATION_COUNT;
-        
-        $( '<div/>', {
-            'id': 'label-' + label,
-            'class': 'label',
-            text: label
-        })
-        .css( 'top', top)
-        .css( 'left', left)
-        .appendTo( markerRef.parent() );
-        
-         */
-        
-        // TODO: probably send over an icon ref for future use
-        $(marker.icon.imageDiv).attr('selected', true);
-        
-        $( '#stationDetail' ).drawerPanel( 'appendContents', contents );
-        $( '#stationDetail' ).drawerPanel( 'open' );
+function clickPoint( point ) {
+    var attr = point.attributes;
     
+    if ( PANEL_STATION_COUNT >= MAX_SELECTED_STATIONS ) {
+        return;
     }
+
+    if ( attr.selected ) {
+        return;
+    }
+    
+    var index = ++PANEL_STATION_COUNT;
+    attr.label = index;
+    attr.selected = true;
+    
+    var sanitizedId = sanitizeString( attr.id );
+    
+    var contents = Mustache.render(
+            DETAIL_TEMPLATE, {
+                id: sanitizedId,
+                index: index,
+                name: attr.name.toCapitalCase(),
+                lat: attr.lat,
+                lon: attr.lon
+            }
+    );
+    
+    stationAndGraphLinkHash[index] = {
+        id: sanitizedId,
+        point: point
+    };
+
+    $( '#stationDetail' ).drawerPanel( 'appendContents', contents );
+    $( '#stationDetail' ).drawerPanel( 'open' );
+
 }
 
 function sanitizeString( input ) {
     return input.replace( /\W/g, ID_DELIMITER );
+}
+
+function removeGraph( index ) {
+    // decrement any items greater than the removed
+    console.log("count is " + PANEL_STATION_COUNT);
+    console.log("selected index is " + index );
+    
+    for (var i = PANEL_STATION_COUNT; i > index ; i--) {
+        
+        var shift = stationAndGraphLinkHash[i];
+        // update graph label
+        var newIndex = i -1;
+        $( 'span.point-index', 'div#' + shift.id + '-detail' ).html( '(' + newIndex + ')' );
+        
+        // update point label
+        shift.point.attributes.label = newIndex;
+        shift.point.layer.redraw();
+    }
+    
+    // remove the items to be removed
+    var point = stationAndGraphLinkHash[index].point;
+    $('div#' + stationAndGraphLinkHash[index].id + '-detail', '#stationDetail' ).remove();
+    point.attributes.label = '';
+    point.attributes.selected = false;
+    point.layer.redraw();
+    delete stationAndGraphLinkHash[index];
+    
+    PANEL_STATION_COUNT--;
 }
 
 String.prototype.toCapitalCase = function( allCapsWordLength ) {
