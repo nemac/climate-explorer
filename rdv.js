06e44f6075ca9d9dfa5fbd35eb35dd49c7605a3d
Jeff Hicks <jmhicks@gmail.com>
2014-05-12 15:10:59 -0400

    Resize other graphs when adding a new one

--- rdv.js
+++ rdv.js
@@ -201,8 +201,8 @@ $(function(){
                 (function(window) {
                     var _jq = window.multigraph.jQuery;
                     var _jqRef = _jq( 'div.graph', '#' + id + '-detail' );
-                    // resize if multigraph exists
-                    if ( _jqRef.html().length > 0 ) {
+                    // resize if multigraph has been deployed
+                    if ( typeof _jqRef.find( 'canvas' ).html() !== 'undefined' && _jqRef.find( 'canvas' ).html().length > 0 ) {
                         var width = _jqRef.width();
                         var height = _jqRef.height();
                         _jqRef.multigraph( 'done', function( m ) {
@@ -258,6 +258,9 @@ $(function(){
             class: 'graph'
         }).appendTo( '#' + id + '-detail' );
         
+        // resize other graphs in case scrollbar appears
+        resizeGraphs();
+        
         // register type
         $.each( stationAndGraphLinkHash, function( i, obj ) {
             if (obj && obj.id === id) {
@@ -265,11 +268,6 @@ $(function(){
                 return false;
             }
         });
-        
-        // resize all other graphs in case a scrollbar will appeared
-        if ( selectedStationCount > 1 ) {
-            //resizeGraphs();
-        }
         
         var payload = MuglHelper.getDataRequests( type, id );
         $.when.apply( $, payload.requests ).done( function(){
