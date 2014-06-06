e7216fca03c5814867a3e8f097e0315cb0bb7038
Mark Phillips <embeepea@git>
2014-06-06 16:09:32 -0400

    move RDV Permalink object definition back to rdv.js, since it is RDV-specific

--- rdv.js
+++ rdv.js
@@ -480,3 +480,116 @@ $(function(){
         });
     }
 });
+
+//
+// RDV Permalink object
+//
+// This object provides a convenient API for translating between permalink URLs and
+// application state.
+// 
+// Usage:
+//    var pl = Permalink(URL(window.location.toString()));
+//
+function Permalink(url) {
+    var center = null, zoom = null, gp = null;
+    var graphs = [];
+    var scales = {};
+    if ('zoom' in url.params) {
+        zoom = parseInt(url.params.zoom, 10);
+    }
+    if ('center' in url.params) {
+        center = url.params.center.split(',').map(function(s) { return parseFloat(s); });
+    }
+    if ('gp' in url.params) {
+        var fields = url.params.gp.split(':');
+        gp = {
+            'open'  : parseInt(fields[0],10) !== 0
+        }
+        if (fields.length > 1) {
+            gp.width = parseInt(fields[1],10);
+        }
+    }
+    if ('graphs' in url.params) {
+        url.params.graphs.split(',').forEach(function(graphString) {
+            var fields = graphString.split(':');
+            graphs.push({id:fields[0], type:fields[1]});
+        });
+    }
+    if ('scales' in url.params) {
+        url.params.scales.split(',').forEach(function(scale) {
+            var fields = scale.split(':');
+            scales[fields[0]] = { min : fields[1], max : fields[2] };
+        });
+    }
+    return {
+        'toString' : function() { return url.toString(); },
+        'haveCenter' : function() { return center !== null; },
+        'getCenter'  : function() { return center; },
+        'setCenter'  : function(c) {
+            center = c;
+            url.params.center = center[0] + "," + center[1];
+        },
+        'haveZoom' : function() { return zoom !== null; },
+        'getZoom'  : function() { return zoom; },
+        'setZoom'  : function(z) {
+            zoom = z;
+            url.params.zoom = zoom.toString();
+        },
+        'haveGp'   : function() { return gp !== null; },
+        'getGp'    : function() { return gp; },
+        'setGp'    : function(g) {
+            gp = g;
+            url.params.gp = gp.open ? "1" : "0";
+            if ('width' in gp) {
+                url.params.gp = url.params.gp + ":" + gp.width;
+            }
+        },
+        'haveGraphs' : function() { return graphs.length > 0; },
+        'getGraphs' : function() { return graphs; },
+        'addGraph' : function(graph) {
+            var i;
+            // don't add this graph if it's already in the list
+            for (i=0; i<graphs.length; ++i) {
+                if (graphs[i].id === graph.id && graphs[i].type == graph.type) {
+                    return;
+                }
+            }
+            graphs.push(graph);
+            url.params.graphs = graphs.map(function(g) { return g.id + ":" + g.type; }).join(",");
+        },
+        'removeGraph' : function(graph) {
+            for ( var i = graphs.length - 1; i >= 0; i-- ) {
+                for ( var j = 0; j < graph.types.length; j++ ) {
+                    if (graphs[i].type === graph.types[j] && graphs[i].id === graph.id) {    
+                        graphs.splice ( i, 1 );
+                        break;
+                    }
+                }   
+            }
+
+            if (graphs.length > 0) {
+                url.params.graphs = graphs.map(function(g) { return g.id + ":" + g.type; }).join(",");
+            } else {
+                delete url.params.graphs;
+            }
+        },
+        'setScales' : function(aR) {
+            var bindingId;
+            for (bindingId in aR) {
+                if (!(bindingId in scales)) {
+                    scales[bindingId] = {};
+                }
+                scales[bindingId].min = aR[bindingId].min;
+                scales[bindingId].max = aR[bindingId].max;
+            }
+            url.params.scales = Object.keys(scales).map(function(bindingId) {
+                return bindingId.replace("-binding", "") + ":" + scales[bindingId].min + ":" + scales[bindingId].max;
+            }).join(",");
+        },
+        'haveScales' : function() {
+            return Object.keys(scales).length > 0;
+        },
+        'getScales' : function() { return scales; }
+    };
+}
+
