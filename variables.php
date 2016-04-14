d1bd0752652482f1da7e018300155e72299ca76b
Brendan Heberton <brendan.heberton@gmail.com>
2016-04-14 16:18:49 -0600

    adding seasons to variable map

--- variables.php
+++ variables.php
@@ -38,7 +38,7 @@
       <ul id="vars-menu" class="menu blue-menu">
         <li class="search-field border"><span class="icon icon-search"></span><input type="text" id="formmapper" placeholder="Search by location"></li>
 
-        <li class="select border">
+        <li class="select border" id="variable-options-container">
           <select class="dropdown" id="variable-options">
             <option value="tasmax">Mean Daily Maximum</option>
             <option value="tasmin">Mean Daily Minimum</option>
@@ -47,6 +47,15 @@
             <option value="pr">Mean Daily Precipitation</option>
             <option value="heating_degree_day_18.3">Heating Degree Days</option>
             <option value="cooling_degree_day_18.3">Cooling Degree Days</option>
+          </select>
+        </li>
+
+        <li class="select border" id="map-seasons-container">
+          <select class="dropdown" id="map-season">
+            <option value="summer">Summer</option>
+            <option value="fall">Fall</option>
+            <option value="winter">Winter</option>
+            <option value="spring">Spring</option>
           </select>
         </li>
 
