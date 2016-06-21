311124042a8bedccddfd92865feb3135e9066343
phil-evans <phil@makeitoperativ.com>
2016-06-21 12:25:20 -0400

    about/defs/credits content & initial styling

--- credits.php
+++ credits.php
@@ -40,20 +40,66 @@
 
         <?php include_once('template/share.php'); ?>
 
-        <div id="location-search">
-          <input type="text" class="autocomplete location-mapper" placeholder="Search another location">
-        </div>
-
-        <section id="location-splash" class="page-splash">
+        <section id="standard-splash" class="page-splash">
             <div class="splash-text">
-                <h1>Credits</h1>
-                <p>This is the credits page.</p>
+                <h1>Credits & Acknowledgments</h1>
             </div>
         </section>
-
-        <!-- begin temperature chart and map -->
-
         
+        <section id="standard-body" class="page-text">
+          
+          <p>The U.S. Climate Resilience Toolkit and Climate Explorer are managed by NOAA’s Climate Program Office, and hosted by NOAA’s National Centers for Environmental Information (NCEI-Asheville).</p>
+          
+          <p>Development of the Climate Explorer was directed and overseen by an interagency team of federal climate modeling experts, chaired by the U.S. Global Change Research Program (see details below).</p>
+          
+          <p>Design and programming were completed by HabitatSeven, in partnership with the National Environmental Modeling and Analysis Center (NEMAC), at UNC-Asheville. </p>
+ 
+          <p>Production and processing of the Bias Corrected Constructed Analog (BCCA) downscaled climate projection data was provided by the U.S. Geological Survey (USGS), in partnership with NASA and NOAA. NASA Earth Exchange Downscaled Climate Projection (NEX DCP30) data were provided by NASA Ames Research Center, and PRISM historical observed data were provided by Oregon State University. </p>
+          
+          <h3>Interagency Climate Projection Team Members</h3>
+          
+          <ul>
+            <li>Fred Lipschultz, Chair, contractor to U.S. Global Change Research Program</li>
+            <li>Jay Alder, U.S. Geological Survey</li>
+            <li>Dave Blodgett, U.S. Geological Survey</li>
+            <li>Subhrendu Gangopadhyay, U.S. Bureau of Reclamation</li>
+            <li>Forrest Melton, NASA Ames Research Center Cooperative for Research in Earth Science and Technology / California State University Monterey Bay</li>
+            <li>Phil Morefield, U.S. Environmental Protection Agency
+Andrea Ray, NOAA</li>
+          </ul>
+          
+          <h3>U.S. Climate Resilience Toolkit Program Management</h3>
+          
+          <ul>
+            <li>David Herring, program manager, NOAA</li>
+            <li>LuAnn Dahlman, managing editor, contractor to NOAA</li>
+            <li>Ned Gardiner, public engagement manager, contractor to NOAA</li>
+            <li>Larry Belcher, programmer, contractor to NOAA</li>
+          </ul>
+          
+          <h3>Design, Programming & Development</h3>
+          
+          <ul>
+            <li>Jamie Herring, president & lead designer, HabitatSeven</li>
+            <li>Jordan Harding, chief technology officer, HabitatSeven</li>
+            <li>Brendan Heberton, application architect, HabitatSeven</li>
+            <li>Aires Almeida, chief creative officer, HabitatSeven</li>
+            <li>Phil Evans, senior designer, HabitatSeven</li>
+            <li>James Fox, director & product manager, NEMAC</li>
+            <li>Jeff Hicks, software engineering, data analysis, NEMAC</li>
+            <li>Ian Johnson, geospatial technician, NEMAC</li>
+            <li>Mark Phillips, software engineering, data analysis, NEMAC</li>
+          </ul>
+          
+          <h3>Thanks also to reviewers:</h3>
+          
+          <ul>
+            <li>Katharine Hayhoe, climate modeler, Texas Tech University</li>
+            <li>Keith Dixon, climate modeler, NOAA Geophysical Fluid Dynamics Laboratory</li>
+            <li>Ken Kunkel, climate modeler, NOAA National Center for Environmental Information</li>
+          </ul>
+                    
+        </section>
     </div>
 </div>
 
