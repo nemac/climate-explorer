8fe5c5b9d01aec9bbfe5fcf8e0e5583b6df6f7e5
John Frimmel <jrfrimme@unca.edu>
2018-05-24 11:03:14 -0400

    adds GA tracking tag

--- template/footer.php
+++ template/footer.php
@@ -37,3 +37,13 @@
     window.app = new App('/resources/data/');
   });
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119744751-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-119744751-1');
+</script>
