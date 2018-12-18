91a4da53a876fa070e7f2d86e94e3e049750f6b8
Chase Martin <chasemartin88@gmail.com>
2018-12-18 16:04:03 -0500

    fixed daily vs climate maps

--- resources/js/station-charts.js
+++ resources/js/station-charts.js
@@ -99,39 +99,46 @@ ChartBuilder.prototype.getData = function (callback) {
 
 ChartBuilder.prototype.buildChart = function () {
   const temps = this.getTemperatureValues();
-  this.temperatureData = temps;
-  const tmpl = this.getTemplate('temperature', temps);
+  this.temperatureData = temps.data;
+  const tmpl = this.getTemplate('temperature', temps.data, temps.min - 10, temps.max + 10, temps.endpor);
   $('#multi-chart').multigraph({'muglString': tmpl});
-  $('#multi-chart').multigraph('done', function (m) { $(window).resize(function () { m.resize();}); });
+  $('#multi-chart').multigraph('done', function (m) {
+    $(window).resize(function () {
+      m.resize();
+    });
+  });
 
 
   const precip = this.getPrecipitationValues();
-  this.precipitationData = precip;
+  this.precipitationData = precip.data;
   //console.log('precip', precip);
-  const precipTmpl = this.getTemplate('precipitation', precip);
+  const precipTmpl = this.getTemplate('precipitation', precip.data, precip.min, precip.max + 5, precip.endpor);
   $('#multi-precip-chart').multigraph({'muglString': precipTmpl});
-  $('#multi-precip-chart').multigraph('done', function (m) { $(window).resize(function () { m.resize();}); });
+  $('#multi-precip-chart').multigraph('done', function (m) {
+    $(window).resize(function () {
+      m.resize();
+    });
+  });
 
 };
 
-ChartBuilder.prototype.downloadTemperatureData = function(link){
-  link.href =  'data:text/csv;base64,' + window.btoa(('date,min,max,normal_min,normal_max,' + '\n' + this.temperatureData.join('\n')));
+ChartBuilder.prototype.downloadTemperatureData = function (link) {
+  link.href = 'data:text/csv;base64,' + window.btoa(('date,min,max,normal_min,normal_max,' + '\n' + this.temperatureData.join('\n')));
   link.download = [
-    this.options.station,
+    this.props.station,
     "temperature",
     'degreeF'
   ].join('-').replace(/ /g, '_') + '.csv';
 };
 
-ChartBuilder.prototype.downloadPrecipitationData = function(link){
-  link.href =  'data:text/csv;base64,' + window.btoa(('date,precipitation,precipitation_normal' + '\n' + this.precipitationData.join('\n')));
+ChartBuilder.prototype.downloadPrecipitationData = function (link) {
+  link.href = 'data:text/csv;base64,' + window.btoa(('date,precipitation,precipitation_normal' + '\n' + this.precipitationData.join('\n')));
   link.download = [
-    this.options.station,
+    this.props.station,
     "precipitation",
     'inch'
   ].join('-').replace(/ /g, '_') + '.csv';
 };
-
 
 
 ChartBuilder.prototype.getTemperatureValues = function () {
@@ -166,6 +173,9 @@ ChartBuilder.prototype.getTemperatureValues = function () {
   enddate = new Date(enddate.slice(0, 4), parseInt(enddate.slice(4, 6) - 1), enddate.slice(6, 8));
   const idate = new Date(startdate.getTime());
   let dmin, dmax, dnormmin, dnormmax, key;
+  let ymin = 0;
+  let ymax = 90;
+  let endpor;
   dmin = dmax = dnormmax = dnormmin = key = '';
   while (idate < enddate) {
     key = [
@@ -176,6 +186,15 @@ ChartBuilder.prototype.getTemperatureValues = function () {
     if (max.hasOwnProperty(key) && min.hasOwnProperty(key)) {
       dmax = max[key];
       dmin = min[key];
+      if (isFinite(dmin)) {
+        ymin = Math.min(ymin, parseFloat(dmin));
+      }
+      if (isFinite(dmax)) {
+        ymax = Math.max(ymax, parseFloat(dmax));
+      }
+      if (parseFloat(dmin) < 0 || parseFloat(dmax) > 0) {
+        endpor = key;
+      }
     }
     let normdate = String(year - ((year - parseInt(key.slice(0, 4))) % 4)) + key.slice(-4);
     if (normmax.hasOwnProperty(normdate) && normmin.hasOwnProperty(normdate)) {
@@ -210,7 +229,7 @@ ChartBuilder.prototype.getTemperatureValues = function () {
     });
   }
 
-  return merge.join('\n');
+  return {data: merge.join('\n'), min: ymin, max: ymax, endpor: endpor};
 };
 
 ChartBuilder.prototype.getPrecipitationValues = function () {
@@ -220,8 +239,7 @@ ChartBuilder.prototype.getPrecipitationValues = function () {
     if (a.indexOf('M') !== -1) {
       if (String(a[0].slice(-5)) === '01-01') {
         a[1] = '0'
-      }
-      else {
+      } else {
         return;
       }
     }
@@ -233,8 +251,7 @@ ChartBuilder.prototype.getPrecipitationValues = function () {
     if (a.indexOf('M') !== -1 || a.indexOf('T') !== -1) {
       if (String(a[0].slice(-5)) === '01-01') {
         a[1] = '0'
-      }
-      else {
+      } else {
         return;
       }
     }
@@ -250,6 +267,9 @@ ChartBuilder.prototype.getPrecipitationValues = function () {
   const idate = new Date(startdate.getTime());
   let dprecip, dnormprecip, key;
   dprecip = dnormprecip = key = '';
+  let ymin = 0;
+  let ymax = 45;
+  let endpor;
   while (idate < enddate) {
     key = [
       idate.getFullYear(),
@@ -258,6 +278,12 @@ ChartBuilder.prototype.getPrecipitationValues = function () {
     ].join('');
     if (precip.hasOwnProperty(key)) {
       dprecip = precip[key];
+      if (isFinite(dprecip)) {
+        ymax = Math.max(ymax, parseFloat(dprecip));
+        if(parseFloat(dprecip) > 0){
+          endpor = key;
+        }
+      }
     }
     let normdate = String(year - ((year - parseInt(key.slice(0, 4))) % 4)) + key.slice(-4);
     if (normprecip.hasOwnProperty(normdate)) {
@@ -281,135 +307,138 @@ ChartBuilder.prototype.getPrecipitationValues = function () {
     });
   }
 
-  return merge.join('\n');
+  return {data: merge.join('\n'), min: ymin, max: ymax, endpor: endpor};
 };
 
-ChartBuilder.prototype.getTemplate = function (type, values) {
-  const today = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+ChartBuilder.prototype.getTemplate = function (type, values, ymin, ymax, endpor) {
+  endpor = endpor || new Date().toISOString().slice(0, 10).replace(/-/g, '');
+  let addMonth = function(d){ d.setMonth(d.getMonth() + 1); return d;}
+  endpor = (addMonth(new Date(endpor.slice(0, 4), parseInt(endpor.slice(4, 6) - 1), endpor.slice(6, 8)))).toISOString().slice(0, 10).replace(/-/g, '');
   //console.log('values', values);
   const templates = {
     'temperature': '<mugl>' +
-    '<plotarea margintop="18"/>' +
-    '<legend rows="1" border="0" opacity="0.0" base="0 1" anchor="0 1" position="0 25">' +
-    '<icon border="0" width="30" height="30"/>' +
-    '</legend>' +
-    '<horizontalaxis id="date" type="datetime" min="' + (new Date().getFullYear() - 2) + '0501" max="' + today + '">' +
-    '<labels spacing="100Y 50Y 20Y 10Y 5Y 1Y 6M 3M 2M 1M 7D 1D" format="%n %d%L%Y"/>' +
-    '<title/>' +
-    '<grid/>' +
-    '<binding id="time-binding" min="19000101" max="20000101"/>' +
-    '</horizontalaxis>' +
-    '<verticalaxis id="temp" min="-10" max="110">' +
-    '<title anchor="0 -1" angle="90" position="-25 0">Degrees (F)</title>' +
-    '<grid/>' +
-    '<labels spacing="100 50 20 10 5 1 0.5 0.2 0.1" format="%f"/>' +
-    '</verticalaxis>' +
-    '<plot>' +
-    '<legend label="Normal Temperature Range"/>' +
-    '<horizontalaxis ref="date">' +
-    '<variable ref="date"/>' +
-    '</horizontalaxis>' +
-    '<verticalaxis ref="temp">' +
-    '<variable ref="normal_mint"/>' +
-    '<variable ref="normal_maxt"/>' +
-    '</verticalaxis>' +
-    '<renderer type="band">' +
-    '<option name="fillcolor" value="0xabdda4"/>' +
-    '<option name="linewidth" value="0"/>' +
-    '<option name="linecolor" value="0xabdda4"/>' +
-    '</renderer>' +
-    '</plot>' +
-    '<plot>' +
-    '<legend label="Actual Temperature Range"/>' +
-    '<horizontalaxis ref="date">' +
-    '<variable ref="date"/>' +
-    '</horizontalaxis>' +
-    '<verticalaxis ref="temp">' +
-    '<variable ref="mint"/>' +
-    '<variable ref="maxt"/>' +
-    '</verticalaxis>' +
-    '<renderer type="rangebar">' +
-    '<option name="fillcolor" value="0x3288bd"/>' +
-    '<option name="barwidth" value="20H"/>' +
-    '<option name="baroffset" value="0.5"/>' +
-    '<option name="linecolor" value="0x3288bd"/>' +
-    '</renderer>' +
-    '</plot>' +
-    '<data>' +
-    '<variables missingvalue="-9000" missingop="le">' +
-    '<variable column="0" id="date" type="datetime"/>' +
-    '<variable column="1" id="maxt"/>' +
-    '<variable column="2" id="mint"/>' +
-    '<variable column="3" id="normal_maxt"/>' +
-    '<variable column="4" id="normal_mint"/>' +
-    '</variables>' +
-    '<values>' +
-    values +
-    '</values>' +
-    '</data>' +
-    '</mugl>',
+      '<plotarea margintop="18"/>' +
+      '<legend rows="1" border="0" opacity="0.0" base="0 1" anchor="0 1" position="0 25">' +
+      '<icon border="0" width="30" height="30"/>' +
+      '</legend>' +
+      '<horizontalaxis id="date" type="datetime" min="' + (parseFloat(endpor.slice(0, 4)) - 2) + '0501" max="' + endpor + '">' +
+      '<labels spacing="100Y 50Y 20Y 10Y 5Y 1Y 6M 3M 2M 1M 7D 1D" format="%n %d%L%Y"/>' +
+      '<title/>' +
+      '<grid/>' +
+      '<binding id="time-binding" min="19000101" max="20000101"/>' +
+      '</horizontalaxis>' +
+      '<verticalaxis id="temp" min="' + ymin + '" max="' + ymax + '">' +
+      '<title anchor="0 -1" angle="90" position="-25 0">Degrees (F)</title>' +
+      '<grid/>' +
+      '<labels spacing="100 50 20 10 5 1 0.5 0.2 0.1" format="%f"/>' +
+      '<pan min="'+ (Math.floor(ymin) - 15) +'" max="'+ (Math.ceil(ymax) + 15) +'" />' +
+      '</verticalaxis>' +
+      '<plot>' +
+      '<legend label="Normal Temperature Range"/>' +
+      '<horizontalaxis ref="date">' +
+      '<variable ref="date"/>' +
+      '</horizontalaxis>' +
+      '<verticalaxis ref="temp">' +
+      '<variable ref="normal_mint"/>' +
+      '<variable ref="normal_maxt"/>' +
+      '</verticalaxis>' +
+      '<renderer type="band">' +
+      '<option name="fillcolor" value="0xabdda4"/>' +
+      '<option name="linewidth" value="0"/>' +
+      '<option name="linecolor" value="0xabdda4"/>' +
+      '</renderer>' +
+      '</plot>' +
+      '<plot>' +
+      '<legend label="Actual Temperature Range"/>' +
+      '<horizontalaxis ref="date">' +
+      '<variable ref="date"/>' +
+      '</horizontalaxis>' +
+      '<verticalaxis ref="temp">' +
+      '<variable ref="mint"/>' +
+      '<variable ref="maxt"/>' +
+      '</verticalaxis>' +
+      '<renderer type="rangebar">' +
+      '<option name="fillcolor" value="0x3288bd"/>' +
+      '<option name="barwidth" value="20H"/>' +
+      '<option name="baroffset" value="0.5"/>' +
+      '<option name="linecolor" value="0x3288bd"/>' +
+      '</renderer>' +
+      '</plot>' +
+      '<data>' +
+      '<variables missingvalue="-9000" missingop="le">' +
+      '<variable column="0" id="date" type="datetime"/>' +
+      '<variable column="1" id="maxt"/>' +
+      '<variable column="2" id="mint"/>' +
+      '<variable column="3" id="normal_maxt"/>' +
+      '<variable column="4" id="normal_mint"/>' +
+      '</variables>' +
+      '<values>' +
+      values +
+      '</values>' +
+      '</data>' +
+      '</mugl>',
     'precipitation': '<mugl>' +
-    '<plotarea margintop="18"/>' +
-    '<legend rows="1" border="0" opacity="0.0" base="0 1" anchor="0 1" position="0 25">' +
-    '<icon border="0" width="30" height="30"/>' +
-    '</legend>' +
-    '<horizontalaxis id="datetime" type="datetime" min="20100101" max="' + today + '">' +
-    '<labels spacing="100Y 50Y 20Y 10Y 5Y 1Y 6M 3M 2M 1M 7D 1D" format="%n %d%L%Y"/>' +
-    '<title/>' +
-    '<grid/>' +
-    '<binding id="time-binding" min="19000101" max="20000101"/>' +
-    '</horizontalaxis>' +
-    '<verticalaxis id="precip" min="0" max="60">' +
-    '<title anchor="0 -1" angle="90" position="-25 0">Inches</title>' +
-    '<grid/>' +
-    '<labels spacing="100 50 20 10 5 1 0.5 0.2 0.1" format="%f"/>' +
-    '<pan min="0" />' +
-    '<zoom min="0" />' +
-    '</verticalaxis>' +
-    '<plot>' +
-    '<legend label="Normal YTD Precipitation"/>' +
-    '<horizontalaxis ref="datetime">' +
-    '<variable ref="datetime"/>' +
-    '</horizontalaxis>' +
-    '<verticalaxis ref="precip">' +
-    '<variable ref="precip_normal"/>' +
-    '</verticalaxis>' +
-    '<legend label="annual"/>' +
-    '<renderer type="pointline">' +
-    '<option name="linecolor" value="#2c3e50"/>' +
-    '<option name="linewidth" value="1.5"/>' +
-    '</renderer>' +
-    '</plot>' +
-    '<plot>' +
-    '<legend label="YTD Precipitation"/>' +
-    '<horizontalaxis ref="datetime">' +
-    '<variable ref="datetime"/>' +
-    '</horizontalaxis>' +
-    '<verticalaxis ref="precip">' +
-    '<variable ref="precip"/>' +
-    '</verticalaxis>' +
-    '<legend visible="false"/>' +
-    '<renderer type="fill">' +
-    '<option name="fillcolor" value="0x3288bd"/>' +
-    '<option name="fillopacity" value="0.5"/>' +
-    '<option name="linecolor" value="0x3288bd"/>' +
-    '</renderer>' +
-    '<datatips format="{0}: {1}">' +
-    '<variable format="%n %y"/>' +
-    '<variable format="%1d"/>' +
-    '</datatips>' +
-    '</plot>' +
-    '<data>' +
-    '<variables missingvalue="-9000" missingop="le">' +
-    '<variable column="0" id="datetime" type="datetime"/>' +
-    '<variable column="1" id="precip"/>' +
-    '<variable column="2" id="precip_normal"/>' +
-    '</variables>' +
-    '<values>' +
-    values +
-    '</values>' +
-    '</data>' +
-    '</mugl>'
+      '<plotarea margintop="18"/>' +
+      '<legend rows="1" border="0" opacity="0.0" base="0 1" anchor="0 1" position="0 25">' +
+      '<icon border="0" width="30" height="30"/>' +
+      '</legend>' +
+      '<horizontalaxis id="datetime" type="datetime" min="'+(parseFloat(endpor .slice(0, 4)) - 2)+'" max="' + endpor + '">' +
+      '<labels spacing="100Y 50Y 20Y 10Y 5Y 1Y 6M 3M 2M 1M 7D 1D" format="%n %d%L%Y"/>' +
+      '<title/>' +
+      '<grid/>' +
+      '<binding id="time-binding" min="19000101" max="20000101"/>' +
+      '</horizontalaxis>' +
+      '<verticalaxis id="precip" min="' + ymin + '" max="' + ymax + '">' +
+      '<title anchor="0 -1" angle="90" position="-25 0">Inches</title>' +
+      '<grid/>' +
+      '<labels spacing="100 50 20 10 5 1 0.5 0.2 0.1" format="%f"/>' +
+      '<pan min="0" max="'+ (Math.round(ymax) + 10) +'" />' +
+      '<zoom min="0" />' +
+      '</verticalaxis>' +
+      '<plot>' +
+      '<legend label="Normal YTD Precipitation"/>' +
+      '<horizontalaxis ref="datetime">' +
+      '<variable ref="datetime"/>' +
+      '</horizontalaxis>' +
+      '<verticalaxis ref="precip">' +
+      '<variable ref="precip_normal"/>' +
+      '</verticalaxis>' +
+      '<legend label="annual"/>' +
+      '<renderer type="pointline">' +
+      '<option name="linecolor" value="#2c3e50"/>' +
+      '<option name="linewidth" value="1.5"/>' +
+      '</renderer>' +
+      '</plot>' +
+      '<plot>' +
+      '<legend label="YTD Precipitation"/>' +
+      '<horizontalaxis ref="datetime">' +
+      '<variable ref="datetime"/>' +
+      '</horizontalaxis>' +
+      '<verticalaxis ref="precip">' +
+      '<variable ref="precip"/>' +
+      '</verticalaxis>' +
+      '<legend visible="false"/>' +
+      '<renderer type="fill">' +
+      '<option name="fillcolor" value="0x3288bd"/>' +
+      '<option name="fillopacity" value="0.5"/>' +
+      '<option name="linecolor" value="0x3288bd"/>' +
+      '</renderer>' +
+      '<datatips format="{0}: {1}">' +
+      '<variable format="%n %y"/>' +
+      '<variable format="%1d"/>' +
+      '</datatips>' +
+      '</plot>' +
+      '<data>' +
+      '<variables missingvalue="-9000" missingop="le">' +
+      '<variable column="0" id="datetime" type="datetime"/>' +
+      '<variable column="1" id="precip"/>' +
+      '<variable column="2" id="precip_normal"/>' +
+      '</variables>' +
+      '<values>' +
+      values +
+      '</values>' +
+      '</data>' +
+      '</mugl>'
   };
 
   return templates[type];
