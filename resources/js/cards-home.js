8933b23e685dfbc086f5f967a38e4ae1ca782caf
Dave Michelson <daveism@gmail.com>
2019-05-29 17:07:04 -0400

    added missing elements on card page

---
+++ resources/js/cards-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(function () {
+
+  $('#default-city-state').text(window.ce.ce('getLocationPageState')['city']);
+  $('#default-city-county').text(window.ce.ce('getLocationPageState')['county']);
+  $('#cards-search-input').val(window.ce.ce('getLocationPageState')['city']);
+
+});
