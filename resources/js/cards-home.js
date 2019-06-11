b59c8661d421f44edda55570e77679fc1cf01de7
Dave Michelson <daveism@gmail.com>
2019-06-11 15:43:55 -0400

    adds card click for local charts
    only local charts working....

--- resources/js/cards-home.js
+++ resources/js/cards-home.js
@@ -1,9 +1,21 @@
 'use strict';
 
 $(function () {
-  
+
   $('#default-city-state').text(window.ce.ce('getLocationPageState')['city']);
   $('#default-city-county').text(window.ce.ce('getLocationPageState')['county']);
   $('#cards-search-input').val(window.ce.ce('getLocationPageState')['city']);
 
+  addCardClick('.card-local-charts');
+
+  // adds a click event to got to card location
+  function addCardClick(selctor) {
+    $(selctor).click( function(e) {
+      const link = document.querySelector(`${selctor} #secretlink`);
+      const url = `${$(link).attr('href')}/${window.location.search}`;
+      $(link).attr('href', url);
+      link.click();
+    });
+
+  }
 });
