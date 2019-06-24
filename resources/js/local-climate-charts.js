5ebf27ec3e41c3c40df038abbfce2df85e58c979
Dave Michelson <daveism@gmail.com>
2019-06-24 11:16:23 -0400

    seperateing out some code for use in other pages.

--- resources/js/local-climate-charts.js
+++ resources/js/local-climate-charts.js
@@ -347,87 +347,8 @@ $(function () {
     });
   }
 
-  // function changes button to selected
-  function toggleButton(selector){
-    toggleAllButtonsOff(selector.get())
-    $(selector).addClass('btn-default-selected');
-  }
-
-  // function changes button to selected
-  function toggleAllButtonsOff(btnElem){
-    const parentElem = btnElem[0].parentElement;
-    const elems = parentElem.childNodes;
-     elems.forEach((elem) => {
-      if (elem instanceof Element) {
-        elem.classList.remove('btn-default-selected');
-        elem.classList.add('btn-default');
-      }
-    });
-  }
-
-  // function enables custom selection dropdown from a li element
-  function enableCustomSelect(uniqueSelector) {
-    const $styledSelect = $(`.select.${uniqueSelector} div.select-styled`);
-
-    // if disabled exit and do not enable pulldown
-    if ( $styledSelect.hasClass( 'disabled' )){
-      return null;
-    }
 
 
-    // enable click and show options
-    $styledSelect.click(function(e) {
-      e.stopPropagation();
-      $(`.select.${uniqueSelector} div.select-styled.active`).not(this).each(function(){
-        $(this).removeClass('active').next('ul.select-options').hide();
-      });
-      $(this).toggleClass('active').next('ul.select-options').toggle();
-    });
-
-    // get list tiems so we can add user interactions
-    var $list = $(`.select.${uniqueSelector} ul`);
-    var $listItems = $(`.select.${uniqueSelector} ul`).children('li');
-
-    // enable click for options
-    $listItems.click(function(e) {
-
-      // check if disabled exit if it is
-      if ($(this).hasClass('default-select-option-disabled')) {return null}
-
-      e.stopPropagation();
-
-      // option item has href make it a element so links work
-      var hrefAttr = $(this).attr('href');
-      if (typeof hrefAttr !== typeof undefined && hrefAttr !== false) {
-        $styledSelect.html(`<a href="${hrefAttr}" rel="${$(this).attr('rel')}">${$(this).text()}</a>`).removeClass('active');
-      } else {
-        $styledSelect.text($(this).text()).removeClass('active');
-      }
-
-      $styledSelect.attr('rel',$(this).attr('rel'))
-
-      // option item has icon add it
-      var iconAttr = $(this).attr('icon');
-      if (typeof iconAttr !== typeof undefined && iconAttr !== false) {
-        // Element has this attribute
-        var icon = `<i class="${iconAttr}"></i>`;
-      } else {
-        var icon = '';
-      }
-
-      $styledSelect.prepend(icon);
-      $list.hide();
-      // trigger custom event so we know the user changed or selected an item
-      $styledSelect.trigger('cs-changed' );
-    });
-
-    // hide pulldown when user clicks anywhere outside of selected area
-    $(document).click(function() {
-        $styledSelect.removeClass('active');
-        $list.hide();
-    });
-
-  }
   // This function will be called whenever the user changes the x-scale in the graph.
   function xrangesetmon() {
     // Force the slider thumbs to adjust to the appropriate place
