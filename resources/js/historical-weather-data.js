3002ddbf1ca529719d279de00f2e381f8a820230
Dave Michelson <daveism@gmail.com>
2019-08-05 16:11:17 -0400

    clean up and comment code

--- resources/js/historical-weather-data.js
+++ resources/js/historical-weather-data.js
@@ -17,6 +17,7 @@ $(function () {
   const stationMOverMHHW = stationsMapState['stationMOverMHHW'];
   const center = [lat, lon]
 
+  // initialize staion map state from url values
   stationsMapState = {
     mode,
     stationId,
@@ -28,41 +29,98 @@ $(function () {
     center
   };
 
+// updates the visible text for the station pulldown with the information from the state url
+function updateStationSelectText(stations) {
+  const stationsSelectElem = $('#stations-select-vis');
+  if (stationsSelectElem) {
+      stationsSelectElem.attr('rel',`${stations.stationId},${stations.stationName}`);
+      stationsSelectElem.text(`${stations.stationName} - (${stations.stationId})`);
+  }
+}
+
+// the way graphs are handled and initialized require them to visible
+// so we move them off the screen.  this not ideal but we can move
+// trade them with the map when we the use needs them
+function resetGraphs(stations) {
+  // remove and reset old graphs
+  $('#stations-graph-wrap').empty();
+
+  // add new graph wrappers so they will initialize
+  $('#stations-graph-wrap').append('<div id="multi-chart" class="left_chart d-flex-center width-50"></div>');
+  $('#stations-graph-wrap').append('<div id="multi-precip-chart" class="left_chart d-flex-center width-50"></div>');
+
+  // update graphs with new station id and station name
+  $('#multi-chart').stationAnnualGraph({ variable: 'temperature', station: stations.stationId, stationName: stations.stationName });
+  $('#multi-precip-chart').stationAnnualGraph({ variable: 'precipitation', station:  stations.stationId, stationName: stations.stationName });
+}
+
+
+// updates the visible text for the station pulldown with the information from the state url
+updateStationSelectText({stationName, stationId})
+
+// show graph overlay.
+// graph is visbile and on page just pushed of viewable area
+// so we can intialize it when needed
+function showGraphs() {
+  const stationsGraphRowElem = document.getElementById('stations-graph-row');
+  const stationsMapRowElem = document.getElementById('stations-map-row');
+
+  // unhide chart overlay
+  if (stationsGraphRowElem) {
+    stationsGraphRowElem.classList.remove('d-off');
+    stationsGraphRowElem.classList.add('d-flex');
+  }
+
+  // hide chart overlay
+  if (stationsMapRowElem) {
+    stationsMapRowElem.classList.add('d-off');
+    stationsMapRowElem.classList.remove('d-flex');
+  }
+}
+
+// show map overlay.
+// map is visbile and on page just pushed of viewable area
+// so we can intialize it when needed
+function showMap() {
+  const stationsGraphRowElem = document.getElementById('stations-graph-row');
+  const stationsMapRowElem = document.getElementById('stations-map-row');
+
+  // unhide chart overlay
+  if (stationsGraphRowElem) {
+    stationsGraphRowElem.classList.remove('d-flex');
+    stationsGraphRowElem.classList.add('d-off');
+  }
+
+  // unhide map overlay
+  if (stationsMapRowElem) {
+    stationsMapRowElem.classList.add('d-flex');
+    stationsMapRowElem.classList.remove('d-off');
+  }
+}
+
+
+// if state url has a station render station and not map.
+if (stationId) {
+  // unhide chart overlay
+  showGraphs()
+
+  // reset graphs
+  resetGraphs({variable: 'temperature', stationId, stationName });;
+
+  // toggle button visual state
+  const selector = $("#chartmap-wrapper #btn-chart")
+  toggleButton($('.btn-chart'));
+
   // updates the visible text for the station pulldown with the information from the state url
   updateStationSelectText({stationName, stationId})
 
-  // if state url has a station render station and not map.
-  if (stationId) {
-    const stationsGraphRowElem = document.getElementById('stations-graph-row');
-    const stationsMapRowElem = document.getElementById('stations-map-row');
-
-    if (stationsGraphRowElem) {
-      stationsGraphRowElem.classList.remove('d-off');
-      stationsGraphRowElem.classList.add('d-flex');
-    }
-
-    if (stationsMapRowElem) {
-      stationsMapRowElem.classList.add('d-off');
-      stationsMapRowElem.classList.remove('d-flex');
-    }
-
-    const options = {variable: 'temperature', station: stationId, stationName:  stationName }
-    // resetGraphs(options);
-
-    $('#multi-chart').stationAnnualGraph({ variable: 'temperature', station: stationId, stationName:  stationName });
-    $('#multi-precip-chart').stationAnnualGraph({ variable: 'precipitation', station:  stationId, stationName:  stationName });
-
-    // toggle button visual state
-    const selector = $("#chartmap-wrapper #btn-chart")
-    toggleButton($('.btn-chart'));
-
-    // updates the visible text for the station pulldown with the information from the state url
-    updateStationSelectText({from: 'start', stationName, stationId})
-
-    setTimeout(function () {
-      setMapSize();
-    }, 600);
-  }
+  setTimeout(function () {
+    // reset map and chart sizes
+    // filer transistion means heigh will be updates in few seconds
+    // so delaying the resize ensures proper size
+    setMapSize();
+  }, 600);
+}
 
   // in resposnive mode, event hanlder a for when season (time) varriable changes
   $('#stations-select-vis').bind('cs-changed', function(e) {
@@ -77,6 +135,7 @@ $(function () {
       document.getElementById('station-info').classList.remove('d-none');
       updateStationIDText(`${stationId}`);
       updateStationText(`${stationName}`);
+
       // change map varriable
       window.ce.ce('setStationsMapState', {stationId, stationName});
 
@@ -91,32 +150,16 @@ $(function () {
         center
       };
 
-      const stationsGraphRowElem = document.getElementById('stations-graph-row');
-      const stationsMapRowElem = document.getElementById('stations-map-row');
+      // unhide chart overlay
+      showGraphs();
 
-      if (stationsGraphRowElem) {
-        stationsGraphRowElem.classList.remove('d-off');
-        stationsGraphRowElem.classList.add('d-flex');
-      }
-
-      if (stationsMapRowElem) {
-        stationsMapRowElem.classList.add('d-off');
-        stationsMapRowElem.classList.remove('d-flex');
-      }
-
-
-      $('#stations-graph-wrap').empty()
-
-      $('#stations-graph-wrap').append('<div id="multi-chart" class="left_chart d-flex-center width-50"></div>');
-      $('#stations-graph-wrap').append('<div id="multi-precip-chart" class="left_chart d-flex-center width-50"></div>');
-
-      $('#multi-chart').stationAnnualGraph({ variable: 'temperature', station: stationId, stationName:  stationName });
-      $('#multi-precip-chart').stationAnnualGraph({ variable: 'precipitation', station:  stationId, stationName:  stationName });
+      // reset graphs
+      resetGraphs({variable: 'temperature', stationId, stationName });;
 
       // toggle button visual state
-      const selector = $("#chartmap-wrapper #btn-chart")
-      // toggleAllButtonsOff(selector.get())
       toggleButton($('.btn-chart'));
+
+      // reset map and chart sizes
       setMapSize();
     }
   })
@@ -134,37 +177,22 @@ $(function () {
       // change select pulldowns for resposnive mode
       setSelectFromButton(target);
 
-
-      const stationsGraphRowElem = document.getElementById('stations-graph-row');
-      const stationsMapRowElem = document.getElementById('stations-map-row');
-
+      // check val of button to see if user is on map  or chart
+      // hide or unhide the appropriate overlay (map, chart)
       switch (target.attr('val')) {
         case 'chart':
-          if (stationsGraphRowElem) {
-            stationsGraphRowElem.classList.remove('d-off');
-            stationsGraphRowElem.classList.add('d-flex');
-          }
-
-          if (stationsMapRowElem) {
-            stationsMapRowElem.classList.add('d-off');
-            stationsMapRowElem.classList.remove('d-flex');
-          }
+          // unhide chart overlay
+          showGraphs();
           break;
         case 'map':
-          if (stationsGraphRowElem) {
-            stationsGraphRowElem.classList.remove('d-flex');
-            stationsGraphRowElem.classList.add('d-off');
-          }
-
-          if (stationsMapRowElem) {
-            stationsMapRowElem.classList.add('d-flex');
-            stationsMapRowElem.classList.remove('d-off');
-          }
+          // unhide map overlay
+          showMap();
           break
         default:
 
       }
     }
+    // reset map and chart sizes
     setMapSize();
   })
 
@@ -178,36 +206,22 @@ $(function () {
       // toggle button visual state
       toggleButton($(`.btn-${$('#chartmap-select-vis').attr('rel')}`));
 
-      const stationsGraphRowElem = document.getElementById('stations-graph-row');
-      const stationsMapRowElem = document.getElementById('stations-map-row');
-
+      // check val of button to see if user is on map  or chart
+      // hide or unhide the appropriate overlay (map, chart)
       switch (target.attr('val')) {
         case 'chart':
-          if (stationsGraphRowElem) {
-            stationsGraphRowElem.classList.remove('d-off');
-            stationsGraphRowElem.classList.add('d-flex');
-          }
-
-          if (stationsMapRowElem) {
-            stationsMapRowElem.classList.add('d-off');
-            stationsMapRowElem.classList.remove('d-flex');
-          }
+          // unhide chart overlay
+          showGraphs();
           break;
         case 'map':
-          if (stationsGraphRowElem) {
-            stationsGraphRowElem.classList.remove('d-flex');
-            stationsGraphRowElem.classList.add('d-off');
-          }
-
-          if (stationsMapRowElem) {
-            stationsMapRowElem.classList.add('d-flex');
-            stationsMapRowElem.classList.remove('d-off');
-          }
+          // unhide map overlay
+          showMap();
           break
         default:
 
       }
     }
+    // reset map and chart sizes
     setMapSize();
   })
 
@@ -262,6 +276,9 @@ $(function () {
     }
 
     setTimeout(function () {
+      // reset map and chart sizes
+      // filer transistion means heigh will be updates in few seconds
+      // so delaying the resize ensures proper size
       setMapSize();
     }, 600);
   })
@@ -279,79 +296,49 @@ $(function () {
     // show graph hide map
     // todo add this to puldown events also
     stationUpdated: function(event, options) {
-      const stationsGraphRowElem = document.getElementById('stations-graph-row');
-      const stationsMapRowElem = document.getElementById('stations-map-row');
-
-      if (stationsGraphRowElem) {
-        stationsGraphRowElem.classList.remove('d-off');
-        stationsGraphRowElem.classList.add('d-flex');
-      }
-
-      if (stationsMapRowElem) {
-        stationsMapRowElem.classList.add('d-off');
-        stationsMapRowElem.classList.remove('d-flex');
-      }
-
+      // unhide chart overlay
+      showGraphs();
 
       // toggle button visual state
       const selectorToggle = $("#chartmap-wrapper #btn-chart")
-      // toggleAllButtonsOff(selector.get())
+
+      // toggle button to select chart
       toggleButton($('.btn-chart'));
 
-      $('#stations-graph-wrap').empty();
-      $('#stations-graph-wrap').append('<div id="multi-chart" class="left_chart d-flex-center width-50"></div>');
-      $('#stations-graph-wrap').append('<div id="multi-precip-chart" class="left_chart d-flex-center width-50"></div>');
-
-      $('#multi-chart').stationAnnualGraph({ variable: 'temperature', station: options.stationId, stationName:  options.stationName });
-      $('#multi-precip-chart').stationAnnualGraph({ variable: 'precipitation', station:  options.stationId, stationName:  options.stationName });
+      // reset graphs
+      resetGraphs({variable: 'temperature', stationId: options.stationId, stationName: options.stationName });;
 
       // updates the visible text for the station pulldown with the information from the state url
       updateStationSelectText({stationName: options.stationName, stationId: options.stationId})
 
-      // resetGraphs(options);
       window.ce.ce('setStationsMapState', options);
 
     }
   }, stationsMapState));
 
-  // updates the visible text for the station pulldown with the information from the state url
-  function updateStationSelectText(stations) {
-    const stationsSelectElem = $('#stations-select-vis');
-    if (stationsSelectElem) {
-        stationsSelectElem.attr('rel',`${stations.stationId},${stations.stationName}`);
-        stationsSelectElem.text(`${stations.stationName} - (${stations.stationId})`);
-    }
-  }
 
-  function resetGraphs(stationId, stationName) {
-    // remove and reset old graphs
-    $('#stations-graph-wrap').empty();
-
-    // add new graph wrappers so they will initialize
-    $('#stations-graph-wrap').append('<div id="multi-chart" class="left_chart d-flex-center width-50"></div>');
-    $('#stations-graph-wrap').append('<div id="multi-precip-chart" class="left_chart d-flex-center width-50"></div>');
-
-    // update graphs with new station id and station name
-    $('#multi-chart').stationAnnualGraph({ variable: 'temperature', station: stationId, stationName });
-    $('#multi-precip-chart').stationAnnualGraph({ variable: 'precipitation', station:  stationId, stationName });
-  }
 
   // resize map when browser is resized
   function setMapSize() {
     $('#stations-map').height($('#stations-map').parent().height())
 
+    // get map parent element - which provides the correct dimensions for the map
     const rect = document.getElementById('stations-map-wrap').getBoundingClientRect();
+
+    // set size of map overlay
     if (document.querySelector('.esri-view-root')) {
       document.querySelector('.esri-view-root').style.minWidth = `${rect.width}px`;
       document.querySelector('.esri-view-root').style.maxWidth = `${rect.width}px`;
       document.querySelector('.esri-view-root').style.height = `${rect.height}px`;
     }
 
+    // set size of map overlay
     if (document.querySelector('.esri-view-user-storage')) {
       document.querySelector('.esri-view-user-storage').style.minWidth = `${rect.width}px`;
       document.querySelector('.esri-view-user-storage').style.maxWidth = `${rect.width}px`;
     }
 
+    // set size of map
     if (document.querySelector('#stations-map')) {
       document.querySelector('#stations-map').style.minWidth = `${rect.width}px`;
       document.querySelector('#stations-map').style.maxWidth = `${rect.width}px`;
@@ -359,7 +346,11 @@ $(function () {
       document.querySelector('#stations-map').style.minWidth = `${rect.height}px`;
     }
 
+
+    // get graph parent element - which provides the correct dimensions for the graph
     const graphRect = document.getElementById('stations-graph-wrap').getBoundingClientRect();
+
+    // set size of temp chart
     if (document.querySelector('#multi-chart')) {
       document.querySelector('#multi-chart').style.minWidth = `${graphRect.width}px`;
       document.querySelector('#multi-chart').style.maxWidth = `${graphRect.width}px`;
@@ -367,6 +358,7 @@ $(function () {
       document.querySelector('#multi-chart').style.minWidth = `${graphRect.height}px`;
     }
 
+    // set size of precip chart
     if (document.querySelector('#multi-precip-chart')) {
       document.querySelector('#multi-precip-chart').style.minWidth = `${graphRect.width}px`;
       document.querySelector('#multi-precip-chart').style.maxWidth = `${graphRect.width}px`;
@@ -375,6 +367,7 @@ $(function () {
     }
   }
 
+  // reset map and chart sizes
   setMapSize();
 
   $(window).resize(function () {
