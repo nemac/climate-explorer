0ee2f833ae3a18d2492615a16af43672769c4fa1
Dave Michelson <daveism@gmail.com>
2019-10-21 14:30:57 -0400

    update thumb nails for new tiles.

--- resources/js/cards-home.js
+++ resources/js/cards-home.js
@@ -2,7 +2,7 @@
 
 $(function () {
 
-  $('#default-city-state').text(window.ce.ce('getLocationPageState')['city']);
+  $('#default-city-state').text(window.ce.ce('getLocationPageState')['city'] || 'Please select a location');
   $('#default-city-county').text(window.ce.ce('getLocationPageState')['county']);
   $('#cards-search-input').val(window.ce.ce('getLocationPageState')['city']);
 
