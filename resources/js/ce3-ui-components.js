e0beebe5cc78a2d6c564d2d508bb18b843eb9574
Dave Michelson <daveism@gmail.com>
2019-10-24 09:16:40 -0400

    adds tab and enter support to pulldowns

--- resources/js/ce3-ui-components.js
+++ resources/js/ce3-ui-components.js
@@ -13,37 +13,97 @@ function reEnableSelectNewItems(uniqueSelector){
   var $listItems = $(`.select.${uniqueSelector} ul`).children('li');
 
   // enable click for options
-  $listItems.click(function(e) {
+  $listItems.keyup(function(e){
+     if (e.keyCode === 13){
+       // check if disabled exit if it is
+       if ($(this).hasClass('default-select-option-disabled')) {return null}
+
+       e.stopPropagation();
+       // option item has href make it a element so links work
+       var hrefAttr = $(this).attr('href');
+       if (typeof hrefAttr !== typeof undefined && hrefAttr !== false) {
+         $styledSelect.html(`<a href="${hrefAttr}" rel="${$(this).attr('rel')}">${$(this).text()}</a>`).removeClass('active');
+       } else {
+         $styledSelect.text($(this).text()).removeClass('active');
+       }
+
+       $styledSelect.attr('rel',$(this).attr('rel'))
+       $styledSelect.attr('link',$(this).attr('link'))
+       $styledSelect.attr('nav',$(this).attr('nav'))
+
+       // option item has icon add it
+       var iconAttr = $(this).attr('icon');
+       if (typeof iconAttr !== typeof undefined && iconAttr !== false) {
+         // Element has this attribute
+         var icon = `<i class="${iconAttr}"></i>`;
+       } else {
+         var icon = '';
+       }
+
+       $styledSelect.prepend(icon);
+       $list.hide();
+       // trigger custom event so we know the user changed or selected an item
+       $styledSelect.trigger('cs-changed' );
+     }
+  });
+
+  // enable key down for options
+  $listItems.click(function(e){
+      // check if disabled exit if it is
+      if ($(this).hasClass('default-select-option-disabled')) {return null}
+
+      e.stopPropagation();
+      // option item has href make it a element so links work
+      var hrefAttr = $(this).attr('href');
+      if (typeof hrefAttr !== typeof undefined && hrefAttr !== false) {
+        $styledSelect.html(`<a href="${hrefAttr}" rel="${$(this).attr('rel')}">${$(this).text()}</a>`).removeClass('active');
+      } else {
+        $styledSelect.text($(this).text()).removeClass('active');
+      }
+
+      $styledSelect.attr('rel',$(this).attr('rel'))
+      $styledSelect.attr('link',$(this).attr('link'))
+      $styledSelect.attr('nav',$(this).attr('nav'))
+
+      // option item has icon add it
+      var iconAttr = $(this).attr('icon');
+      if (typeof iconAttr !== typeof undefined && iconAttr !== false) {
+        // Element has this attribute
+        var icon = `<i class="${iconAttr}"></i>`;
+      } else {
+        var icon = '';
+      }
+
+      $styledSelect.prepend(icon);
+      $list.hide();
+      // trigger custom event so we know the user changed or selected an item
+      $styledSelect.trigger('cs-changed' );
+  });
+
+
+  var $listTips = $(`.select.${uniqueSelector} ul`).children('span');
+
+  $listTips.click(function(e) {
     // check if disabled exit if it is
     if ($(this).hasClass('default-select-option-disabled')) {return null}
-
     e.stopPropagation();
 
-    // option item has href make it a element so links work
-    var hrefAttr = $(this).attr('href');
-    if (typeof hrefAttr !== typeof undefined && hrefAttr !== false) {
-      $styledSelect.html(`<a href="${hrefAttr}" rel="${$(this).attr('rel')}">${$(this).text()}</a>`).removeClass('active');
-    } else {
-      $styledSelect.text($(this).text()).removeClass('active');
+    const relAttr = $(this).attr('rel');
+    const testItem = $(`li[rel='${relAttr}']`);
+    $(testItem).click();
+  });
+
+
+  $listTips.keyup(function(e) {
+    if (e.keyCode === 13){
+      // check if disabled exit if it is
+      if ($(this).hasClass('default-select-option-disabled')) {return null}
+      e.stopPropagation();
+
+      const relAttr = $(this).attr('rel');
+      const testItem = $(`li[rel='${relAttr}']`);
+      $(testItem).click();
     }
-
-    $styledSelect.attr('rel',$(this).attr('rel'))
-    $styledSelect.attr('link',$(this).attr('link'))
-    $styledSelect.attr('nav',$(this).attr('nav'))
-
-    // option item has icon add it
-    var iconAttr = $(this).attr('icon');
-    if (typeof iconAttr !== typeof undefined && iconAttr !== false) {
-      // Element has this attribute
-      var icon = `<i class="${iconAttr}"></i>`;
-    } else {
-      var icon = '';
-    }
-
-    $styledSelect.prepend(icon);
-    $list.hide();
-    // trigger custom event so we know the user changed or selected an item
-    $styledSelect.trigger('cs-changed' );
   });
 }
 
@@ -54,6 +114,22 @@ function enableCustomSelect(uniqueSelector) {
   if ( $styledSelect.hasClass( 'disabled' )){
     return null;
   }
+
+  // enable click and show options
+  $styledSelect.keyup(function(e) {
+    if (e.keyCode === 13){
+      if ( $(e.target).hasClass( 'disabled' )){
+        return null;
+      }
+
+      $(`.select.${uniqueSelector} div.select-styled.active`).not(this).each(function(){
+        $(this).removeClass('active').next('ul.select-options').hide();
+      });
+      $(this).toggleClass('active').next('ul.select-options').toggle();
+    }
+
+    e.stopPropagation();
+  });
 
   // enable click and show options
   $styledSelect.click(function(e) {
@@ -74,36 +150,71 @@ function enableCustomSelect(uniqueSelector) {
   var $listItems = $(`.select.${uniqueSelector} ul`).children('li');
 
   // enable click for options
-  $listItems.click(function(e) {
-    // check if disabled exit if it is
-    if ($(this).hasClass('default-select-option-disabled')) {return null}
-    e.stopPropagation();
+  $listItems.keyup(function(e){
+     if (e.keyCode === 13){
+       // check if disabled exit if it is
+       if ($(this).hasClass('default-select-option-disabled')) {return null}
 
-    // option item has href make it a element so links work
-    const hrefAttr = $(this).attr('href');
-    if (typeof hrefAttr !== typeof undefined && hrefAttr !== false) {
-      $styledSelect.html(`<a href="${hrefAttr}" rel="${$(this).attr('rel')}">${$(this).text()}</a>`).removeClass('active');
-    } else {
-      $styledSelect.text($(this).text()).removeClass('active');
-    }
+       e.stopPropagation();
+       // option item has href make it a element so links work
+       var hrefAttr = $(this).attr('href');
+       if (typeof hrefAttr !== typeof undefined && hrefAttr !== false) {
+         $styledSelect.html(`<a href="${hrefAttr}" rel="${$(this).attr('rel')}">${$(this).text()}</a>`).removeClass('active');
+       } else {
+         $styledSelect.text($(this).text()).removeClass('active');
+       }
 
-    $styledSelect.attr('rel',$(this).attr('rel'))
-    $styledSelect.attr('link',$(this).attr('link'))
-    $styledSelect.attr('nav',$(this).attr('nav'))
+       $styledSelect.attr('rel',$(this).attr('rel'))
+       $styledSelect.attr('link',$(this).attr('link'))
+       $styledSelect.attr('nav',$(this).attr('nav'))
 
-    // option item has icon add it
-    const iconAttr = $(this).attr('icon');
-    if (typeof iconAttr !== typeof undefined && iconAttr !== false) {
-      // Element has this attribute
-      var icon = `<i class="${iconAttr}"></i>`;
-    } else {
-      var icon = '';
-    }
+       // option item has icon add it
+       var iconAttr = $(this).attr('icon');
+       if (typeof iconAttr !== typeof undefined && iconAttr !== false) {
+         // Element has this attribute
+         var icon = `<i class="${iconAttr}"></i>`;
+       } else {
+         var icon = '';
+       }
 
-    $styledSelect.prepend(icon);
-    $list.hide();
-    // trigger custom event so we know the user changed or selected an item
-    $styledSelect.trigger('cs-changed' );
+       $styledSelect.prepend(icon);
+       $list.hide();
+       // trigger custom event so we know the user changed or selected an item
+       $styledSelect.trigger('cs-changed' );
+     }
+  });
+
+  // enable key down for options
+  $listItems.click(function(e){
+      // check if disabled exit if it is
+      if ($(this).hasClass('default-select-option-disabled')) {return null}
+
+      e.stopPropagation();
+      // option item has href make it a element so links work
+      var hrefAttr = $(this).attr('href');
+      if (typeof hrefAttr !== typeof undefined && hrefAttr !== false) {
+        $styledSelect.html(`<a href="${hrefAttr}" rel="${$(this).attr('rel')}">${$(this).text()}</a>`).removeClass('active');
+      } else {
+        $styledSelect.text($(this).text()).removeClass('active');
+      }
+
+      $styledSelect.attr('rel',$(this).attr('rel'))
+      $styledSelect.attr('link',$(this).attr('link'))
+      $styledSelect.attr('nav',$(this).attr('nav'))
+
+      // option item has icon add it
+      var iconAttr = $(this).attr('icon');
+      if (typeof iconAttr !== typeof undefined && iconAttr !== false) {
+        // Element has this attribute
+        var icon = `<i class="${iconAttr}"></i>`;
+      } else {
+        var icon = '';
+      }
+
+      $styledSelect.prepend(icon);
+      $list.hide();
+      // trigger custom event so we know the user changed or selected an item
+      $styledSelect.trigger('cs-changed' );
   });
 
   var $listTips = $(`.select.${uniqueSelector} ul`).children('span');
@@ -117,6 +228,20 @@ function enableCustomSelect(uniqueSelector) {
     const testItem = $(`li[rel='${relAttr}']`);
     $(testItem).click();
   });
+
+
+  $listTips.keyup(function(e) {
+    if (e.keyCode === 13){
+      // check if disabled exit if it is
+      if ($(this).hasClass('default-select-option-disabled')) {return null}
+      e.stopPropagation();
+
+      const relAttr = $(this).attr('rel');
+      const testItem = $(`li[rel='${relAttr}']`);
+      $(testItem).click();
+    }
+  });
+
 
   // hide pulldown when user clicks anywhere outside of selected area
   $(document).click(function() {
