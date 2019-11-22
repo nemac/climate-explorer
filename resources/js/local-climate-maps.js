15f27b7823804bdf4893b0ab5e6de37d1fc2f32c
Dave Michelson <daveism@gmail.com>
2019-11-22 16:18:27 -0500

    fix spelling. :)

--- resources/js/local-climate-maps.js
+++ resources/js/local-climate-maps.js
@@ -704,7 +704,7 @@ $(function () {
               // messsageElem.style.top = `-${((rect.bottom - rect.top)/2)}px`;
               messsageElem.style.left = `${(rect.right - rect.left)/3}px`;
               messsageElem.style.top = `-${((rect.bottom - rect.top)-6)}px`;
-              messsageElem.innerHTML = 'The location on the map is outside the Contiental United States, there is currently no climate map data avaialble.'
+              messsageElem.innerHTML = 'The location on the map is outside the Continental United States. Currently, there is no climate map data available.'
               messsageElem.classList.remove('d-none');
           } else {
             messsageElem.classList.add('d-none');
