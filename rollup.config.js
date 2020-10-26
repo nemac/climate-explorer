9debd8e9e16a99f75546a1d4520443ea6b6379d2
Joshua Wilson <jwilson8767@gmail.com>
2020-10-26 18:34:37 -0400

    v3.1.0 - CBL update, area inspection popup, and new build tools

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,18 @@
 // rollup.config.js
 import babel from '@rollup/plugin-babel';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-
-const production =  process.env.build_env === 'production'
+import nodeResolve from '@rollup/plugin-node-resolve';
+let production = process.env.build_env === 'prod' || process.env.build_env === 'production'
 const plugins = [
   nodeResolve(),
-  babel({
-    exclude: 'node_modules/**', // only transpile our source code
-    babelHelpers: 'bundled'
-  })
 ];
+if (production){
+  plugins.push(
+      babel({
+        exclude: 'node_modules/**', // only transpile our source code
+        babelHelpers: 'bundled',
+      })
+  )
+}
 export default [
   {
     input: 'resources/js/local-climate-maps.js',
@@ -21,6 +24,4 @@ export default [
     },
     plugins: plugins
   },
-
-
 ];
