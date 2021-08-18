29318c2486dd2d1c302604521f207f77f4e6600a
Shayan Ghofrany <irantwomiles@gmail.com>
2021-08-18 15:13:19 -0400

    added 'climate explorer' to the nav bar

--- template/secondary_header.js
+++ template/secondary_header.js
@@ -8,6 +8,8 @@ export default (config) => `
       <i class="fas fa-home"></i>
     </a>
     
+    <h2>Climate Explorer</h2>
+    
     <div class="ms-auto d-flex flex-row">
     
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#aboutDataDropdown" aria-controls="aboutDataDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -40,7 +42,7 @@ export default (config) => `
               <a class="nav-link dropdown-toggle" href="#" id="socialMediaDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <span class="fa fa-share-alt"></span>
               </a>
-              <ul class="dropdown-menu" aria-labelledby="socialMediaDropdownMenuLink">
+              <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="socialMediaDropdownMenuLink">
                 <li><a class="dropdown-item" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fcrt-climate-explorer.nemac.org%2Fstations%2F%3Fid%3Dhigh_tide_flooding%26extent%3D-86.27%252C-71.21%252C32.47%252C38.43%26zoom%3D7%26station-mhhw%3D0.54" target="_blank"><span class="icon-facebook me-1"></span> Facebook</a></li>
                 <li><a class="dropdown-item" href="https://twitter.com/intent/tweet?text=https%3A%2F%2Fcrt-climate-explorer.nemac.org%2Fstations%2F%3Fid%3Dhigh_tide_flooding%26extent%3D-86.27%252C-71.21%252C32.47%252C38.43%26zoom%3D7%26station-mhhw%3D0.54" target="_blank"><span class="icon-twitter me-1"></span> Twitter</a></li>
                 <li><a class="dropdown-item" href="#"><span class="icon-link me-1"></span> Copy Permalink</a></li>
